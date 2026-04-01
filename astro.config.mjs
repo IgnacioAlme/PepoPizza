661982ad7fd7ece5d4e1e2f49c8638156d240202
IgnacioAlme <ignacio.yair.almenar@gmail.com>
2026-04-01 09:49:34 -0300

    fix: updated astro config for github pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pepopizza.com',
+  site: 'https://ignacioalme.github.io',
+  base: '/PepoPizza',
   output: 'static',
+  trailingSlash: 'always',
 });
